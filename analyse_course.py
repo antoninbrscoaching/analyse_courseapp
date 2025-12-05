3034d3eb82bcc5f438550d8e9733c9f84233bdc4
antoninbrscoaching <antoninbrscoaching@gmail.com>
2025-12-05 17:32:08 +0100

    Update analyse_course.py

--- analyse_course.py
+++ analyse_course.py
@@ -653,7 +653,8 @@ for r in refs_raw:
 
     # temps sous conditions idéales
     t_ideal = recalibrate_ref_to_ideal(
-        r=r, k_up=k_up, k_down=k_down, 
+        distance=r["distance"], D_up=r["D_up"], D_down=r["D_down"],
+        k_up=k_up, k_down=k_down,
         k_temp_hot=k_temp_hot, k_temp_cold=k_temp_cold, opt_temp=opt_temp
     )
 
@@ -664,7 +665,8 @@ for r in refs_raw:
             lat_input, lon_input, datetime.combine(date_course, heure_course)
         )
         t_hist = recalibrate_ref_using_current(
-            r=r, k_up=k_up, k_down=k_down,
+            distance=r["distance"], D_up=r["D_up"], D_down=r["D_down"],
+            k_up=k_up, k_down=k_down,
             k_temp_hot=k_temp_hot, k_temp_cold=k_temp_cold,
             opt_temp=opt_temp, assumed_temp=temp_hist
         )
